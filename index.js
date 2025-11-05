4222dc9011c6aa4192936a34f3888bb605f22d8e
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-05 08:52:56 +0100

    refactor: use constants instead of hardcoded values

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
-
+const { PORT } = require('./constants/app');
 // create the express application
 const app = express();
 
 // Add middleware to parse JSON
 app.use(express.json());
-
-// setup port and server
-const PORT = process.env.PORT || 2800;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

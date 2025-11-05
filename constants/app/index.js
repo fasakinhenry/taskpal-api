4ea61ba415373c4ed40e7a5583b0894e18dc1882
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-05 08:50:46 +0100

    chore: setup app constants and export them

---
+++ constants/app/index.js
@@ -0,0 +1,20 @@
+const GLOBAL_RATE_LIMIT_WINDOW_MS = 60 * 1000;
+const GLOBAL_REQUEST_PER_MINUTE = 100;
+const ALLOWED_FILE_UPLOAD_EXTENSIONS = ['png', 'jpeg', 'jpg'];
+const MAX_FILE_SIZE = 1024 * 1024 * 5;
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const MORGAN_CONFIG = IS_PRODUCTION
+  ? ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
+  : 'dev';
+
+const PORT = process.env.PORT || 2800;
+
+module.exports = {
+  GLOBAL_RATE_LIMIT_WINDOW_MS,
+  GLOBAL_REQUEST_PER_MINUTE,
+  ALLOWED_FILE_UPLOAD_EXTENSIONS,
+  MAX_FILE_SIZE,
+  IS_PRODUCTION,
+  MORGAN_CONFIG,
+  PORT,
+};

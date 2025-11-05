528e0cdc590d30e1498c3372e19d240549cd8ca5
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-05 13:32:54 +0100

    chore: rename files to suit convention

---
+++ controllers/task.controller.js
@@ -0,0 +1,9 @@
+const Task = require('../models/task.models');
+
+class TaskController {
+  static async createTask(req, res) {}
+  static async login(req, res) {}
+  static async me(req, res) {}
+}
+
+module.exports = TaskController;

462ff8187aa38ad7951a801b0bac91fae512f66d
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-06 15:05:47 +0100

    feat: add code for mongodb data connection

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const { PORT } = require('./constants/app');
+const connectToDB = require('./database/db');
 // create the express application
 const app = express();
+
+// Connect to database
+connectToDB();
 
 // Add middleware to parse JSON
 app.use(express.json());

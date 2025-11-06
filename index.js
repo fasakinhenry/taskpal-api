04007b8e6fb9917d2095506ff049f9508ba8ac9f
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-06 15:07:37 +0100

    feat: add auth routes to the main file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const { PORT } = require('./constants/app');
 const connectToDB = require('./database/db');
+const authRoutes = require('./routes/auth.route');
+
 // create the express application
 const app = express();
 
@@ -10,6 +12,9 @@ connectToDB();
 
 // Add middleware to parse JSON
 app.use(express.json());
+
+// Handle routing
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

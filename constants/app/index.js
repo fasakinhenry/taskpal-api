0ae584d03a045f6a606ed4b3d641b0cfd2acddac
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-07 03:21:33 +0100

    chore: update the value of JWT SECRET

--- constants/app/index.js
+++ constants/app/index.js
@@ -10,7 +10,7 @@ const MORGAN_CONFIG = IS_PRODUCTION
 const PORT = process.env.PORT || 2800;
 
 // JWT specific constants
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'replace_this_with_a_strong_secret';
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'JWT_SECRET_KEY';
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '2d'; // 2 days
 const BCRYPT_SALT_ROUNDS = Number(process.env.BCRYPT_SALT_ROUNDS) || 10;
 

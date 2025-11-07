df99dd6577fc3d9172017664c6c9b820a99d0961
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-07 05:08:13 +0100

    feat: create controller to fetch all tasks posted

---
+++ controllers/user.controller.js
@@ -0,0 +1,33 @@
+const response = require('../utils/response');
+const User = require('../models/User');
+const Task = require('../models/Task');
+
+class UserController {
+  // GET /api/users/me/tasks (my posted & accepted)
+  static async myTasks(req, res, next) {
+    try {
+      const userId = req.user._id;
+
+      const postedTasks = await Task.find({ postedBy: userId });
+      const acceptedTasks = await Task.find({ acceptedBy: userId });
+      return response(res, 200, "User's tasks fetched successfully", {
+        postedTasks,
+        acceptedTasks,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  // GET /api/users/:id/tasks/posted
+  static async postedTasks(req, res, next) {
+    try {
+        const { id } = req.params;
+        const tasks = await Task.find({ createdBy: id }).sort({ createdAt: -1 });
+        return response(res, 200, 'Posted tasks fetched successfully', {tasks});
+    } catch (error) {
+        next(error);
+    }
+  }
+}
+
+module.exports = UserController;

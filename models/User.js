fd1ef1c6a3fa1eef6393eaefcc555f7969db2aeb
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-07 03:29:23 +0100

    feat: update the User schema with right types

--- models/User.js
+++ models/User.js
@@ -5,12 +5,14 @@ const UserSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, 'Name is required'],
+      trim: true,
     },
     email: {
       type: String,
       required: [true, 'Email is required'],
       unique: true,
       trim: true,
+      lowercase: true,
     },
     password: {
       type: String,
@@ -18,24 +20,26 @@ const UserSchema = new mongoose.Schema(
       minLength: [6, 'Password cannot be less than 6 characters'],
     },
     isVerified: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-    skills: {
-      type: String,
-    },
+    skills: [{ type: String }],
     rating: {
       type: Number,
       default: 0,
     },
-    tasksPosted: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Task',
-    },
-    tasksAccepted: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Task',
-    },
+    tasksPosted: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task',
+      },
+    ],
+    tasksAccepted: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task',
+      },
+    ],
   },
   { timestamps: true }
 );
